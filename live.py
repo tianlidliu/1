c2bc0bc4efd931bbcc15006436aabf56a287946f
Ted Liu <tliu@roblox.com>
2023-03-29 02:45:51 -0700

    Some ideas for where to take this

--- live.py
+++ live.py
@@ -1,4 +1,7 @@
 from alpaca.data.live import CryptoDataStream
+
+# 1. Instead of hardcoding these in the python file, try reading these from a file (referred to as a secrets file)
+# 2. Look up the use of .gitignore and make sure that the secrets file is in the gitignore so that you don't accidentally commit it upstream
 API_KEY = ""
 SECRET_KEY = ""
 
@@ -12,4 +15,7 @@ async def bar_callback(bar):
 symbol = "BTC/USD"
 crypto_stream.subscribe_bars(bar_callback, symbol)
 
+
+# 1. Look into the crypto stream API reference on alpaca and see if you can output the results to a file on your computer
+# 2. (And this is a big one...) Instead of a file, see if you can get this to write into a database. We can do this one together on a conference call when you're ready for it. 
 crypto_stream.run()
